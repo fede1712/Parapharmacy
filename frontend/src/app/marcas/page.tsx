da4b87fe6277f84f8f487ab4fe796b297dbdcaa3
Fede Marinas <fedemarinasruiz@gmail.com>
2024-10-31 08:51:15 +0100

    repair build

--- frontend/src/app/marcas/page.tsx
+++ frontend/src/app/marcas/page.tsx
@@ -1,14 +1,21 @@
 "use client";
-import Link from "next/link";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { Brand, ResponseBrand } from "./types/brand.type";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useUpdateEffect } from "@/utils/useUpdateEffect";
+import Link from "next/link";
 
-export default function BrandsPage() {
+function SearchBrands({
+  fetchBrands,
+  data,
+  searchParams,
+}: {
+  fetchBrands: (term: string) => Promise<void>;
+  data: Brand[];
+  searchParams: URLSearchParams;
+}) {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const [data, setData] = useState<Brand[]>([]);
+
   const [inputSearchTerm, setInputSearchTerm] = useState("");
   const [debounceTimer, setDebounceTimer] = useState<NodeJS.Timeout | null>(null);
 
@@ -22,6 +29,65 @@ export default function BrandsPage() {
       fetchBrands("");
     }
   }, [searchParams]);
+
+  useUpdateEffect(() => {
+    if (debounceTimer) {
+      clearTimeout(debounceTimer);
+    }
+
+    const timer = setTimeout(() => {
+      if (inputSearchTerm) {
+        router.push(`/marcas/?search=${inputSearchTerm}`, undefined);
+        fetchBrands(inputSearchTerm);
+      } else {
+        fetchBrands("");
+        router.push("/marcas", undefined);
+      }
+    }, 500);
+    setDebounceTimer(timer);
+
+    return () => clearTimeout(timer);
+  }, [inputSearchTerm, router]);
+
+  return (
+    <div className="grid grid-cols-1 p-6">
+      <input
+        type="text"
+        placeholder="Buscar marca..."
+        value={inputSearchTerm}
+        onChange={(e) => setInputSearchTerm(e.target.value)}
+        className="w-full p-2 mb-4 border rounded-md shadow-md sm:mx-12 focus:outline-none focus:border-blue-500"
+      />
+      <div className="grid gap-2 sm:grid-cols-5 sm:gap-4 sm:mx-12">
+        {data?.map((brand) => {
+          return (
+            <Link
+              key={brand.documentId}
+              href={`/marcas/${brand.slug}`}
+              className="flex items-center justify-between w-full p-4 border rounded-lg cursor-pointer hover:text-green-600 hover:bg-gray-100"
+            >
+              <h3 className="p-4 text-xs font-bold">{brand.name}</h3>
+              <img src={brand.image} alt={brand.name} className="object-contain w-12 h-12 rounded-full" />
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default function BrandsPage() {
+  const [data, setData] = useState<Brand[]>([]);
+
+  return (
+    <Suspense fallback={<p>Cargando marcas..</p>}>
+      <BrandsPageContent setData={setData} data={data} />
+    </Suspense>
+  );
+}
+
+function BrandsPageContent({ setData, data }: { setData: (data: Brand[]) => void; data: Brand[] }) {
+  const searchParams = useSearchParams();
 
   const fetchBrands = async (term: string) => {
     let url;
@@ -46,48 +112,5 @@ export default function BrandsPage() {
     );
   };
 
-  useUpdateEffect(() => {
-    if (debounceTimer) {
-      clearTimeout(debounceTimer);
-    }
-
-    const timer = setTimeout(() => {
-      if (inputSearchTerm) {
-        router.push(`/marcas/?search=${inputSearchTerm}`, undefined);
-        fetchBrands(inputSearchTerm);
-      } else {
-        fetchBrands("");
-        router.push("/marcas", undefined);
-      }
-    }, 500);
-    setDebounceTimer(timer);
-
-    return () => clearTimeout(timer);
-  }, [inputSearchTerm, router]);
-
-  return (
-    <div className="p-6 grid grid-cols-1">
-      <input
-        type="text"
-        placeholder="Buscar marca..."
-        value={inputSearchTerm}
-        onChange={(e) => setInputSearchTerm(e.target.value)}
-        className="w-full p-2 sm:mx-12 mb-4 border rounded-md shadow-md focus:outline-none focus:border-blue-500"
-      />
-      <div className="grid sm:grid-cols-5 gap-2 sm:gap-4 sm:mx-12">
-        {data?.map((brand) => {
-          return (
-            <Link
-              key={brand.documentId}
-              href={`/marcas/${brand.slug}`}
-              className="w-full flex p-4 cursor-pointer hover:text-green-600 border rounded-lg items-center justify-between hover:bg-gray-100"
-            >
-              <h3 className="font-bold text-xs p-4">{brand.name}</h3>
-              <img src={brand.image} alt={brand.name} className="rounded-full h-12 w-12 object-contain" />
-            </Link>
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <SearchBrands data={data} fetchBrands={fetchBrands} searchParams={searchParams} />;
 }
